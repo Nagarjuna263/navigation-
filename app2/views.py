c74a430f51806d144d751640909c76a9c0da5c66
Nagarjuna <nagarjunareddy263@gmail.com>
2022-11-30 11:22:05 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def queen(request):
+    return render(request,'queen.html')
+
+
+def marry(request):
+    return render(request,'marry.html')
